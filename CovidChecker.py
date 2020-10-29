b1046ab1d7ab268fef266696036b398b0c69d182
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2020-10-29 21:08:56 +0000

    Updated and added some more comments. Added more TODO for future progress.

--- CovidChecker.py
+++ CovidChecker.py
@@ -2,7 +2,14 @@
 # CovidChecker.py - A program to check the new coronavirus cases in Scotland
 # Data is taken from - https://www.gov.scot/publications/coronavirus-covid-19-trends-in-daily-data/
 
-
+# TODO: Remove unnecessary prints from WHOLE program
+# TODO: Program should only need an internet connection
+#  IF there is no already existing excel file to use.
+#  Or to download a fresh version.
+#  So it should start, check for a file, if no file exists, try to get a fresh file. If it fails, let exception handle.
+#  If there is a file, but its not with today's date, try to get a fresh file, if fails, exception handles it.
+#  If there is a file and you cant get a new file, post a message and just use older data
+#  If there is a file and its with today's date, use that!
 # pip installs
 # bs4, requests, openpyxl
 
@@ -26,7 +33,7 @@ def getFormattedDate(formatted=True):
     dayNum = today.strftime("%d")   # Returns today's date in a int
     month = today.strftime("%B")    # Returns today's month in word format
     year = today.strftime("%Y")     # Returns year in 4 digits
-    # Will be used to return formatted date to add to URL string later
+    # Check if formatting is required or not, and return the value as a String
     if formatted:
         dateStr = dayNum + "%2B" + month + "%2B" + year
     else:
@@ -36,21 +43,28 @@ def getFormattedDate(formatted=True):
 
 # Downloads a file from the given URL
 def getFile(url, file):
-    print("Downloading the most recent data from the Scottish Gov website")
+    # TODO: Find a way to handle files better
 
     # Check if a file with that name already exists, if not, download a fresh copy
+    # Uses the os module to check the name of the file to download, against all the files in the ExcelFiles directory
     if file in os.listdir(os.getcwd() + '\\ExcelFiles\\'):
         print('A file with today\'s date already exists, using that.')
     else:
+        # TODO: Other exceptions; download fails, no internet connection (this should not be needed if a file exists
         try:
-            print('Downloading...')     # TODO: Remove unnecessary prints
+            # Attempts to use the urllib module to download the given file from the Scot Gov website
+            print('Downloading the new data')
             urllib.request.urlretrieve(url, file)   # Downloads to the same directory as the python file
+            # TODO: If a new file is downloaded, delete the other excel files
+        # If there is an issue with the given URL, display an error, the given URL and end the program
         except urllib.error.URLError:
             print("Error! The given URL is incorrect, please check the URL")
             print("URL Given - " + url)
             print('Ending program.')
-            sys.exit()
+            sys.exit()              # Ends the program as the URL failed # TODO: Change this once better filing is added
         print("File downloaded!")
+        # Move the newly downloaded file into the correct diretory
+        # TODO: Add some sort of error in case moving the file returns an error
         shutil.move(newestFile, 'ExcelFiles')
 
 
@@ -139,14 +153,36 @@ def printNewCases(locations, cases, newestDate):
     for x in range(len(locations)):
         print(locations[x] + ' | ' + cases[x])
 
+
+def getLocationName(location, locationsList):
+    name = ''
+    for x in range(len(locationsList)):
+        if location in locationsList[x]:
+            name = locationsList[x]
+    if name == '':
+        print('Error - Invalid name given, please enter a name that matches one of the following:')
+        print(getLocations())
+        print('Ending program')
+        sys.exit()
+
+    elif name in locationsList:
+        return name
+
+
 # Variables
 newestFile = getFileNameFromLinks()
 today = getFormattedDate()      # Gets the date in a URL format to add to the source file URL
 # TODO: Find a better way to format this URL in the IDE to stop it complaining
-fileURL = "http://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/coronavirus-covid-19-trends-in-daily-data/documents/covid-19-data-by-nhs-board/covid-19-data-by-nhs-board/govscot%3Adocument/COVID-19%2Bdaily%2Bdata%2B-%2Bby%2BNHS%2BBoard%2B-%2B" + today + ".xlsx?forceDownload=true"
+fileURL = "http://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/coronavirus-covid" \
+          "-19-trends-in-daily-data/documents/covid-19-data-by-nhs-board/covid-19-data-by-nhs-board/govscot" \
+          "%3Adocument/COVID-19%2Bdaily%2Bdata%2B-%2Bby%2BNHS%2BBoard%2B-%2B" + today + ".xlsx?forceDownload=true "
 columnLtr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 lastColLtr = columnLtr[-1]
 lastColNum = 16
+# Shorter versions of each of possible locations - Saves typing in the full name
+quickNames = ['Ayrshire', 'Arran', 'Ayrshire & Arran', 'Borders', 'Dumfries', 'Galloway', 'Fife', 'Forth', 'Valley',
+              'Forth Valley', 'Grampian', 'Greater Glasgow', 'Greater Glasgow & Clyde', 'Glasgow', 'Clyde', 'Highland',
+              'Lanarkshire', 'Lothian', 'Orkney', 'Shetland', 'Tayside', 'Western Isles', 'Scotland']
 
 
 # Start by checking if the file is available
@@ -168,6 +204,12 @@ newestDate = str(newestDate.value)[:10]
 
 # Prints all of the new cases from the most recent value
 # printNewCases(getLocations(), getNewest(lastRow), newestDate)
+# print(getLocations())
+
+
+
+
+
+
 
 # TODO: Is this needed? urllib.request.urlcleanup() - https://docs.python.org/3/library/urllib.request.html
-# last = sheet.cell(row=lastRow, column=16)
