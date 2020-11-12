5854bf67daa3ffadb13da226d81ce05ec5f0c9e8
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2020-11-12 01:37:58 +0000

    Removed a bunch of old functions that are no longer suited. They were only there for testing intial access and handling. Added better comments for each function, but they need changed to meet Python standards. Added a function to handle outputting all of the case data in a neater form (can print a list of health boards then their cases, or a specific health board and its case cases. Renamed the getLocations and getLocationName function, to make them clearer that it gets the health board names.

--- CovidChecker.py
+++ CovidChecker.py
@@ -26,9 +26,14 @@ from send2trash import send2trash
 
 
 # Functions
-# Default parameter is used to return the String in the required format to get the correct URL
-# Passing False is used to get a un-formatted String - e.g. 25October2020
 def getFormattedDate(formatted=True):
+    """
+    Returns the data in one of 2 formatted options.
+    Parameters:
+        formatted (Boolean): True returns the data as expected for a URL, otherwise returns DDMMYY
+    Return:
+        dateStr (str): A str representing the formatted date - e.g. 25October2020
+    """
     # Get date in DDMMYYYY format
     today = date.today()  # Create an object to get the date details
     dayNum = today.strftime("%d")  # Returns today's date in a int
@@ -36,15 +41,21 @@ def getFormattedDate(formatted=True):
     year = today.strftime("%Y")  # Returns year in 4 digits
     # Check if formatting is required or not, and return the value as a String
     if formatted:
+        # Returns data, formatted to be used as part of a URL
         dateStr = dayNum + "%2B" + month + "%2B" + year
     else:
+        # Returns today's date in format - DDMonthYear, e.g. 25October2020
         dateStr = dayNum + month + year
     return dateStr
 
 
-# Downloads a file from the given URL
 def getFile(url, file):
-
+    """
+    Downloads a file from the given URL
+    Parameters:
+        url (str): The URL of the Scottish Govs covid data website
+        file (str): The name of the file to look for on the Scottish Gov website
+    """
     # Check if a file with that name already exists, if not, download a fresh copy
     # Uses the os module to check the name of the file to download, against all the files in the ExcelFiles directory
     if file in os.listdir(os.getcwd() + '\\ExcelFiles\\'):
@@ -66,8 +77,12 @@ def getFile(url, file):
         shutil.move(newestFile, 'ExcelFiles')
 
 
-# Scans a web page for all links, returning them in a list
 def getAllLinks():
+    """
+    Scans a web page for all URLs, returning them in a list
+    Return:
+        links (list): A list of all the URLs found on the Scot Gov COVID-19 data website
+    """
     # The website that hosts the files
     url = 'https://www.gov.scot/publications/coronavirus-covid-19-trends-in-daily-data/'
     # Create a soup object, that reads the HTML content and create a list to later store the results
@@ -81,8 +96,12 @@ def getAllLinks():
     return links
 
 
-# Take a list of ULRs and return the file name in a readable format
 def getFileNameFromLinks():
+    """
+    Returns a String to be used as the file name, for the downloaded file
+    Return:
+        filename (str): A name based of the currently available COVID-19 file on the Scot Gob website
+    """
     links = getAllLinks()  # Get a list of URLs to search through for the Excel file
     fileName = ''  # The intended filename for the download object
 
@@ -107,13 +126,28 @@ def getFileNameFromLinks():
         # Print warning message to user
         print('Error! Found no file to download. Please check the URL has a valid file to download.\nIt is possible '
               'the file name has changed.')
+        sys.exit()
 
 
 # ------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------
 
-# Returns a list of each Health Boards name, according to the data available in the sheet
+# A function to handle outputting error messages for each function
+# Did this to avoid having similar ending statements in each function
+# TODO: Use this for now, but I do need to add proper error handling throughout the program
+#  Meaning this WILL be deleted at the end!
+def endIt(errorMsg):
+    print(errorMsg)
+    print('Ending the program')
+    sys.exit()
+
+
 def getHealthBoardList():
+    """
+    Returns a list of each Health Boards names, according to the column data in the Excel sheet
+    Return:
+        healthBoardsList (list): A list of each Health Boards name
+    """
     healthBoardsList = []                           # Used to store all the health board names
     # Loops from the first available health board name (col 2) to the last health board name (col 16)
     for row in sheet.iter_rows(min_row=3, min_col=2, max_row=3, max_col=16):
@@ -123,63 +157,118 @@ def getHealthBoardList():
 
 
 def getNewDataFromAllBoards():
-    newDataList = []
-    for col in range(2, 17):
-        cell = sheet.cell(row=sheet.max_row, column=col)
-        newDataList.append(cell.value)
-    return newDataList
+    """
+    Returns the most recent case totals for each health board
+    Returns:
+        newDataList (list): A list of each health boards case totals
+    """
+    newDataList = []                                        # The list to store the case values
+    for col in range(2, 17):                                # Loop each health board area & Scottish total
+        cell = sheet.cell(row=sheet.max_row, column=col)    # Store the cell for the current health board on the row
+        newDataList.append(cell.value)                      # Get the data for that cell  (stores case numbers)
+    return newDataList                                      # Returns all the case values in the list
 
 
 def getNewScotlandTotal():
+    """
+    Returns the Scottish total cases from the Excel data
+    Return:
+        (int): Total cases in Scotland
+    """
+    # Reads the last column of data, on the last row of data, and returns that cells value
     return sheet.cell(row=sheet.max_row, column=16).value
 
 
 def getSpecificHealthBoardNewCases(healthBoard):
-    # Get the column number of that health board
-    for col in range(2, 17):
-        if sheet.cell(row=3, column=col).value == healthBoard:
-            column = col
-    newData = getNewDataFromAllBoards()
-    return newData[column-2]
+    """
+    Takes a Health Boards full name, and returns its total cases
+    Parameters:
+        healthBoard (str): The name of the health board (Should match that of the Excel sheet)
+    Return:
+        (int): The data from the column for the given health board
+    """
+    columnNum = getHealthBoardColumnNum(healthBoard)    # Gets the column number, for the given health board
+    newData = getNewDataFromAllBoards()                 # Stores all of the newest available total cases in a list
+    return newData[columnNum]                   # From the list of data, select the element from the given column num
+
+
+def getHealthBoardColumnNum(healthBoard):
+    """
+    Takes a health boards full name, and returns the column number from the Excel sheet
+    Parameters:
+        healthBoard (str): The name of the health board (Should match that of the Excel sheet)
+    Return:
+        (int): The column number of the health board
+    """
+    for col in range(2, 17):            # Loops all of the columns of health boards
+        if sheet.cell(row=3, column=col).value == healthBoard:  # Check if the current health board matches the request
+            # Remove 2 from the column number, as the column in excel, starts at 2
+            # so removing 2 the first element back to 1, so it becomes the 'first' column again
+            return col - 2              # Returns the column number
+
+
+def getHealthBoardCasesOverPeriod(timePeriod, healthBoard='all'):
+    """
+    Returns case numbers over a given period, for either all health boards or the specifically requested health board
+    Parameters:
+        timePeriod (int): A number representing how many days of data to look back over
+        healthBoard (str): The name of health board, or 'all' or blank, to get every health board
+    Return:
+        healthBoardList (list): A list for each health boards cases over the requested period
+        (int): The value for the cases over the requested time period
+    """
+
+    # 39 was selected due to formatting in the Gov file
+    # When cases were less than 5, as '*' was displayed, for disclosure reasons
+    # So, to avoid a ValueError, 39 was the first row of data to not have an *
+    # TODO: Find a way to accommodate the asterix value
+    max = sheet.max_row - 39
+    try:
+        length = int(timePeriod)
+    except:
+        print('ERROR: You must give a number between 1 and ' + str(max) + '\nEnding Program.')
+        sys.exit()
+    if int(length) < 1 or int(length) > max:
+        errorMsg = 'ERROR: Given value is not valid for data available. Please enter a value between 1 and ' + str(max)
+        endIt(errorMsg)
+
+    if healthBoard == 'all':
+        print('Getting all health boards cases over ' + str(length) + ' days')  # TODO Remove this after list handling
+        healthBoardList = []
+        for col in range(2, 17):
+            newCell = sheet.cell(row=sheet.max_row, column=col).value
+            olderCell = sheet.cell(row=sheet.max_row - length, column=col).value
+            data = newCell - olderCell
+            healthBoardList.append(data)
+        return healthBoardList
+    else:
+        healthBoard = getHealthBoardFullName(healthBoard)
+        print('Getting the last ' + str(length) + ' days of cases for ' + healthBoard)  # TODO Remove after CMD handling
+        healthBoardColNum = getHealthBoardColumnNum(healthBoard)
+        newCell = sheet.cell(row=sheet.max_row, column=healthBoardColNum).value
+        olderCell = sheet.cell(row=sheet.max_row - length, column=healthBoardColNum).value
+        return newCell - olderCell
+
+
+def outputHandler(locations, values):
+    if type(locations) == list and type(values) == list:
+        print('Received a list, so it should be ALL health boards')
+        maxLenElement = max(locations, key=len)
+        for x in range(len(locations)):
+            spacing = ' ' * (len(maxLenElement) - len(locations[x]) + 2)
+            print(locations[x] + spacing + ' | ' + str(values[x]))
+
+    elif type(locations) == str and type(values) == int:
+        print('Received a str, so it will just be a single health board')
+        print(locations + '\t|\t' + str(values))
+
 
 # ------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------
-
-# TODO: If using this, clean up format so its clear
-# Prints the newest row of data, alongside all of their health board titles
-def printNewestCasesOnly(lastRow):
-    for col in range(2, 17):  # Loop from column 2 (first health board) to 17 (Scottish total)
-        cell = sheet.cell(row=lastRow, column=col)  # Grabs the specific cell each increment of the loop
-        # If it is column A, only show the date, not the time
-        # Otherwise just print the data for each column
-        if col == 1:
-            print(str(cell.value)[:10] + ' | ', end='')
-        else:
-            print(str(cell.value) + ' | ', end='')
-
-
-# Returns a list of the most recent case data
-# Requires the row number of the most recent data
-def getNewestCases(lastRow):
-    newCasesList = []  # Stores the row of data
-    for col in range(2, 17):  # Loops from column A - P
-        cell = sheet.cell(row=lastRow, column=col)  # Each increment, get the cell data from that column
-        newCasesList.append(str(cell.value))  # Add the cells value to the list
-    return newCasesList  # Return the list
-
-
-# TODO: Can this be merged with the above function - pass it how many rows to go back
-def getSecondNewestCases(lastRow):
-    secondNewList = []  # Stores the row of data
-    for col in range(2, 17):  # Loops from column A - P
-        cell = sheet.cell(row=lastRow-1, column=col)  # Each increment, get the cell data from that column
-        secondNewList.append(str(cell.value))  # Add the cells value to the list
-    return secondNewList  # Return the list
-
 
 # Reads the list of health boards and returns them in a list or prints them
 # Depending if printIt is True or not
-def getLocations(printIt=False):
+def getHealthBoardsList(printIt=False):
     locationsList = []  # Used to store the health board names
     for col in range(2, 17):  # Loop columns A - P
         cell = sheet.cell(row=3, column=col)  # Grab the cell from the current column per increment
@@ -196,7 +285,7 @@ def getLocations(printIt=False):
 # Takes a health board name, and returns the exact spelling/format of that health board
 # Used to allow more user friendly input of names - e.g. Grampian becomes NHS Grampian
 # Expects a name to check for and a list of locations to compare against
-def getLocationName(location):
+def getHealthBoardFullName(location):
     locationsList = getHealthBoardList()
     healthBoardNameFull = ''                        # Stores the health board name
     for x in range(len(locationsList)):             # Loops the entire list of health boards
@@ -204,32 +293,11 @@ def getLocationName(location):
             healthBoardNameFull = locationsList[x]  # Store that full health board name in a variable
     if healthBoardNameFull == '':                   # If there is no match, print error, print valid names and end
         print('Error - Invalid name given, please enter a name that matches one of the following:')
-        print(getLocations(printIt=True))           # Prints the health board names
+        print(getHealthBoardsList(printIt=True))           # Prints the health board names
         print('Ending program')
         sys.exit()                                  # Uses the sys package to end the program, as no valid name given
     elif healthBoardNameFull in locationsList:      # If there is a matching name in the location list, return as String
         return healthBoardNameFull
-
-
-def printAllTotals(healthBoards, todaysCases):
-    for x in range(len(healthBoards)):
-        print(healthBoards[x] + ' | ' + todaysCases[x])
-
-
-def getNewTodayAll():
-    today = getNewestCases(getLastRowNumber(sheet))
-    yesterday = getSecondNewestCases(getLastRowNumber(sheet))
-    new = []
-    for values in range(len(today)):
-        difference = int(today[values]) - int(yesterday[values])
-        new.append(str(difference))
-    return new
-
-
-def printLocationsAndValues(caseList):
-    locations = getLocations(False)
-    for x in range(len(locations)):
-        print(locations[x] + ' | ' + caseList[x])
 
 
 # Code
@@ -250,25 +318,12 @@ fileURL = "http://www.gov.scot/binaries/content/documents/govscot/publications/s
           "-19-trends-in-daily-data/documents/covid-19-data-by-nhs-board/covid-19-data-by-nhs-board/govscot" \
           "%3Adocument/COVID-19%2Bdaily%2Bdata%2B-%2Bby%2BNHS%2BBoard%2B-%2B" + today + ".xlsx?forceDownload=true "
 
-# A list of letters representing the columns used in the Excel file
-columnLtr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-lastColLtr = columnLtr[-1]              # The last column letter stored as a String
-lastColNum = 16                         # The column number of the last letter
-
-# Shorter versions of each of possible locations - Saves typing in the full name for users convience
-# TODO - What was the purpose of this again - Getting the full name from the sheet and interpreting the users input,
-#  is the better way to do it
-# quickNames = ['ayrshire', 'arran', 'ayrshire & arran', 'borders', 'dumfries', 'galloway', 'gife', 'forth', 'valley',
-#              'forth valley', 'grampian', 'greater glasgow', 'greater glasgow & clyde', 'glasgow', 'clyde', 'highland',
-#               'lanarkshire', 'lothian', 'orkney', 'shetland', 'tayside', 'western isles', 'scotland']
-
-
 # Start by checking if the file is available
-print("Starting!")
+print("Checking for new data...")
 getFile(fileURL, newestFile)    # Downloads newest file is available or uses older file
 
 # File management - Clear out any other older files
-print('Checking if there are any old files to clear.')
+print('Checking if there are any old files to clear...')
 count = 0
 for theFile in os.listdir(os.getcwd() + '\\ExcelFiles\\'):
     if theFile != newestFile:
@@ -277,8 +332,10 @@ for theFile in os.listdir(os.getcwd() + '\\ExcelFiles\\'):
         send2trash(os.getcwd() + '\\ExcelFiles\\' + theFile)
 if count >= 1:
     print('Deleted ' + str(count) + ' files.')
+else:
+    print('No files to clear.')
 
-# Loads the most recent excel file, data_only used to ignore any formulas
+# Loads the most recent excel file, data_only used to ignore any formulas, as we only need the actual values
 excel = load_workbook('ExcelFiles//' + newestFile, data_only=True)
 
 # Get the correct sheet from the Excel file
@@ -288,45 +345,26 @@ for theSheet in range(len(excel.sheetnames)):
 sheet = excel.active                        # Stores the active sheet in a usable variable
 
 lastRowNum = sheet.max_row
-# lastRow = getLastRowNumber(sheet)                     # Store the last row number
 newestDate = sheet.cell(row=lastRowNum, column=1)  # Get the data of the most available row of data
 newestDate = str(newestDate.value)[:10]         # Remove the end of the String, as it stores an unusable time value
 
 
 def testALL():
-    print(lastRowNum)
-    print()
-    print('--------------------')
-    print()
-    printNewestCasesOnly(lastRowNum)
-    print()
-    print('--------------------')
-    print()
-    print(getNewestCases(lastRowNum))
-    print()
-    print('--------------------')
-    print()
-    print(getLocations())
-    print()
-    print('--------------------')
-    print()
-    getLocations(True)
-    print()
-    print('--------------------')
-    print()
-    print(getLocationName('Highland', getLocations()))
-    print()
-    print('--------------------')
-    print()
-    printAllTotals(getLocations(), getNewestCases(lastRowNum))
+    return None
 
+
+# outputHandler(getHealthBoardFullName('Grampian'), getSpecificHealthBoardNewCases(getHealthBoardFullName('Grampian')))
+# print()
+# outputHandler(getHealthBoardList(), getHealthBoardCasesOverPeriod(19))
+
+print(getHealthBoardCasesOverPeriod.__doc__)
 
 print('\nEnding before checking CMD input')
 sys.exit()
 
 if str(sys.argv[1]).lower() == 'today':
     # if str(sys.argv[2]).lower() in quickNames:
-    #     print('checking todays values for ' + getLocationName(str(sys.argv[2])))
+    #     print('checking todays values for ' + getHealthBoardFullName(str(sys.argv[2])))
     printLocationsAndValues(getNewTodayAll())
 
 # TODO: Is this needed? urllib.request.urlcleanup() - https://docs.python.org/3/library/urllib.request.html
